15c117e47418daa24dccc8b79686cfd670e657a8
Rakhat <radilhanov@list.ru>
2023-04-20 13:05:52 +0600

    fix: adjustments for optimization

--- VirtuMerce.Dal/Providers/EntityFramework/CategoryProvider.cs
+++ VirtuMerce.Dal/Providers/EntityFramework/CategoryProvider.cs
@@ -7,10 +7,7 @@ namespace VirtuMerce.Dal.Providers.EntityFramework;
 
 public class CategoryProvider : BaseProvider<CategoryEntity>, ICategoryProvider
 {
-    private readonly ApplicationContext _applicationContext;
-
     public CategoryProvider(ApplicationContext applicationContext) : base(applicationContext)
     {
-        _applicationContext = applicationContext;
     }
 }
--- VirtuMerce.Dal/Providers/EntityFramework/ProductProvider.cs
+++ VirtuMerce.Dal/Providers/EntityFramework/ProductProvider.cs
@@ -6,10 +6,7 @@ namespace VirtuMerce.Dal.Providers.EntityFramework;
 
 public class ProductProvider : BaseProvider<ProductEntity>, IProductProvider
 {
-    private readonly ApplicationContext _applicationContext;
-    
     public ProductProvider(ApplicationContext applicationContext) : base(applicationContext)
     {
-        _applicationContext = applicationContext;
     }
 }
